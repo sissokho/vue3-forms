f35deec8284ec3942d4a05430826d02657ecf938
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2024-06-19 12:08:03 +0000

    Base Input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    default: ''
+  }
+})
+
+const model = defineModel()
+</script>
+
+<template>
+  <label>{{ label }}</label>
+  <input v-model="model" :placeholder="label" class="field" v-bind="$attrs" />
+</template>
