2c1eecaae5c84b2ec7404c934ecc1bb04f221727
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2024-06-20 11:03:20 +0000

    Validating at form level

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,22 +1,43 @@
 <script setup>
 import BaseInput from '@/components/BaseInput.vue'
 import BaseButton from '@/components/BaseButton.vue'
-import { useField } from 'vee-validate'
+import { useField, useForm } from 'vee-validate'
 
-const { value: email, errorMessage: emailError } = useField('email', (value) => {
-  if (!value) {
-    return 'This field is required'
+const validations = {
+  email: (value) => {
+    if (!value) {
+      return 'This field is required'
+    }
+
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (!regex.test(String(value).toLowerCase())) {
+      return 'Please enter a valid email address'
+    }
+
+    return true
+  },
+  password: (value) => {
+    const requiredMessage = 'This field is required'
+    if (value === undefined || value === null) {
+      return requiredMessage
+    }
+
+    if (!String(value).length) {
+      return requiredMessage
+    }
+
+    return true
   }
+}
 
-  const regex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-  if (!regex.test(String(value).toLowerCase())) {
-    return 'Please enter a valid email address'
-  }
-
-  return true
+useForm({
+  validationSchema: validations
 })
+
+const { value: email, errorMessage: emailError } = useField('email')
+const { value: password, errorMessage: passwordError } = useField('password')
 
 function onSubmit() {
   alert('Submitted')
@@ -27,7 +48,7 @@ function onSubmit() {
   <form @submit.prevent="onSubmit">
     <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput label="Password" type="password" />
+    <BaseInput label="Password" type="password" v-model="password" :error="passwordError" />
 
     <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
